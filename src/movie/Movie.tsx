cf26207fe998c265e2740a38802ad66e9ad68393
ACAZUMAH <calebazumah9@gmail.com>
2025-09-23 14:34:36 +0000

    finishes

--- src/movie/Movie.tsx
+++ src/movie/Movie.tsx
@@ -1,12 +1,16 @@
 import { useRoute } from "@react-navigation/native";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { icons } from "src/constants/icons";
 import { useAppFetch } from "src/hooks/useAppFetch";
+import { useAppNavigation } from "src/hooks/useAppNavigation";
 import { fetchMovieDetails } from "src/services";
+import { MovieInfo } from "./components/MovieInfo";
 
 export const Movie = () => {
   const { params } = useRoute();
+
+  const { navigateBack } = useAppNavigation();
 
   //@ts-ignore
   const { data, loading, error } = useAppFetch(() =>fetchMovieDetails(params?.id));
@@ -38,11 +42,56 @@ export const Movie = () => {
           <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
             <Image source={icons.star} className="size-4" />
             <Text className="text-white font-bold text-sm">
-                {Math.round(data?.vote_average ?? 0)} / 10
+              {Math.round(data?.vote_average ?? 0)} / 10
+            </Text>
+            <Text className="text-light-200 text-sm">
+              ({data?.vote_count} votes)
             </Text>
           </View>
+
+          <MovieInfo label="Overview" value={data?.overview} />
+
+          <MovieInfo
+            label="Genres"
+            value={data?.genres?.map((g) => g.name).join(" . ") || "N/A"}
+          />
+
+          <MovieInfo
+            label="Countries"
+            value={
+              data?.production_countries?.map((c) => c.name).join(" . ") ||
+              "N/A"
+            }
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${data?.budget! / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(data?.revenue!) / 1_000_000} million`}
+            />
+          </View>
+
+          <MovieInfo label="Tagline" value={data?.tagline} />
+
+          <MovieInfo
+            label="Production Companies"
+            value={data?.production_companies.map((c) => c.name).join(" . ")}
+          />
         </View>
       </ScrollView>
+      <TouchableOpacity onPress={() => navigateBack()} className="absolute bottom-6 left-0 right-0 mx-5 bg-accent rounded-full 
+      flex flex-row items-center justify-center z-50 py-3.5">
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor="#fff"
+        />
+        <Text className="text-white font-semibold text-base">Back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/save/index.tsx
+++ src/save/index.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react'
-import { Text, View } from 'react-native'
+import * as React from "react";
+import { Image, Text, View } from "react-native";
+import { icons } from "src/constants/icons";
 
 export const Save = () => {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-5xl text-light-100 font-bold'>Save</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image
+          source={icons.save}
+          className="size-20 rounded-full bg-dark-100 p-5"
+          tintColor="#fff"
+        />
+        <Text className="text-gray-500 text-base">Save Upcoming</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
